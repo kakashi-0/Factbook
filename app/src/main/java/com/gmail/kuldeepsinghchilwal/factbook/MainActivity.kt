4decaf5eafb3615e420e2d63e36295b5e4e36102
kakashi-0 <gchilwal510@gmail.com>
2021-02-24 13:42:52 +0530

    implemented Banner adv
    Signed-off-by: kakashi-0 <gchilwal510@gmail.com>

--- app/src/main/java/com/gmail/kuldeepsinghchilwal/factbook/MainActivity.kt
+++ app/src/main/java/com/gmail/kuldeepsinghchilwal/factbook/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.gmail.kuldeepsinghchilwal.factbook
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.gmail.kuldeepsinghchilwal.factbook.databinding.ActivityMainBinding
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest.Builder
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var drawerLayout: DrawerLayout
-    private lateinit var viewModel: HomeFragmentViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.nav_host_fragment_container)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.navView,navController)
+        MobileAds.initialize(this) {}
+        val adRequest = Builder().build()
+        binding.publisherAdView.loadAd(adRequest)
     }
 
     override fun onSupportNavigateUp(): Boolean {
