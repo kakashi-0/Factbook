59e232566b04b8a7ee0b46afbf30124ad3c4f6c6
kakashi-0 <gchilwal510@gmail.com>
2021-03-02 19:39:44 +0530

    implemented Interstitial adv, have some bugs regarding showing ad
    Signed-off-by: kakashi-0 <gchilwal510@gmail.com>

---
+++ app/src/main/java/com/gmail/kuldeepsinghchilwal/factbook/InterstitialAd.kt
@@ -0,0 +1,67 @@
+package com.gmail.kuldeepsinghchilwal.factbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.gmail.kuldeepsinghchilwal.factbook.databinding.ActivityInterstitialAdBinding
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import com.google.android.gms.ads.admanager.AdManagerInterstitialAd
+import com.google.android.gms.ads.admanager.AdManagerInterstitialAdLoadCallback
+
+class InterstitialAd : AppCompatActivity() {
+    lateinit var binding: ActivityInterstitialAdBinding
+    //Ad
+    private var mAdManagerInterstitialAd: AdManagerInterstitialAd? = null
+    private final var TAG = "Interstitial"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_interstitial_ad)
+        //ad
+        var InterstitialAdRequest = AdManagerAdRequest.Builder().build()
+
+
+            AdManagerInterstitialAd.load(
+                this,
+                "/6499/example/interstitial",
+                InterstitialAdRequest,
+                object : AdManagerInterstitialAdLoadCallback() {
+                    override fun onAdFailedToLoad(adError: LoadAdError) {
+                        Log.d(TAG, adError?.message)
+                        mAdManagerInterstitialAd = null
+                    }
+
+                    override fun onAdLoaded(interstitialAd: AdManagerInterstitialAd) {
+                        Log.d(TAG, "Ad was loaded.")
+                        mAdManagerInterstitialAd = interstitialAd
+                    }
+                })
+            var adRequest = AdManagerAdRequest.Builder().build()
+
+            AdManagerInterstitialAd.load(
+                this,
+                "/6499/example/interstitial",
+                adRequest,
+                object : AdManagerInterstitialAdLoadCallback() {
+                    override fun onAdFailedToLoad(adError: LoadAdError) {
+                        Log.d(TAG, adError?.message)
+                        mAdManagerInterstitialAd = null
+                    }
+
+                    override fun onAdLoaded(interstitialAd: AdManagerInterstitialAd) {
+                        Log.d(TAG, "Ad was loaded.")
+                        mAdManagerInterstitialAd = interstitialAd
+                    }
+                })
+            binding.watchAD.setOnClickListener {
+                    if (mAdManagerInterstitialAd != null) {
+                        mAdManagerInterstitialAd?.show(this)
+                        Toast.makeText(this, "Showing Ad", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Log.d("TAG", "The interstitial ad wasn't ready yet.")
+                        Toast.makeText(this, "Loading Ad please wait ", Toast.LENGTH_SHORT).show()                    } }
+            }
+    }
--- app/src/main/java/com/gmail/kuldeepsinghchilwal/factbook/MainActivity.kt
+++ app/src/main/java/com/gmail/kuldeepsinghchilwal/factbook/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.gmail.kuldeepsinghchilwal.factbook
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.gmail.kuldeepsinghchilwal.factbook.databinding.ActivityMainBinding
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.admanager.AdManagerAdRequest
+import com.google.android.gms.ads.admanager.AdManagerInterstitialAd
+import com.google.android.gms.ads.admanager.AdManagerInterstitialAdLoadCallback
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest.Builder
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -16,16 +28,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.nav_host_fragment_container)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
-        NavigationUI.setupWithNavController(binding.navView,navController)
+        NavigationUI.setupWithNavController(binding.navView, navController)
         MobileAds.initialize(this) {}
-        val adRequest = Builder().build()
-        binding.publisherAdView.loadAd(adRequest)
-    }
+        val bannerAdRequest = Builder().build()
+        binding.publisherAdView.loadAd(bannerAdRequest)
 
+
+    }
+    //navigation menu and nav bar back button
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.nav_host_fragment_container)
         return NavigationUI.navigateUp(navController,drawerLayout)
